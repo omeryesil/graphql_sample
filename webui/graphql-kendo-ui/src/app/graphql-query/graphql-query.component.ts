87b9ac34f6b0171ce6d5ab710f5af1366983a1a4
Omer Yesil <oyesil@cetaris.com>
2024-07-18 13:43:45 -0400

    Works with the grid

--- webui/graphql-kendo-ui/src/app/graphql-query/graphql-query.component.ts
+++ webui/graphql-kendo-ui/src/app/graphql-query/graphql-query.component.ts
@@ -1,5 +1,7 @@
 // src/app/graphql-query/graphql-query.component.ts
 
+// src/app/graphql-query/graphql-query.component.ts
+
 import { Component } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
@@ -7,20 +9,23 @@ import gql from 'graphql-tag';
 @Component({
   selector: 'app-graphql-query',
   templateUrl: './graphql-query.component.html',
-  styleUrls: ['./graphql-query.component.css']
+  // Uncomment the next line if you have a CSS file
+  // styleUrls: ['./graphql-query.component.css']
 })
 export class GraphqlQueryComponent {
-  query: string = 'query: PurchaseOrderHeaders { }';
+  query: string = '';
   data: any[] = [];
   columns: string[] = [];
 
   constructor(private apollo: Apollo) {}
 
   executeQuery() {
-    this.apollo.query({
+    this.apollo.query<any>({
       query: gql`${this.query}`
-    }).subscribe((result: any) => {
-      this.data = result.data ? Object.values(result.data)[0] as any[] : [];
+    }).subscribe((result) => {
+      this.data = result.data && result.data.purchaseOrderHeaders && result.data.purchaseOrderHeaders.nodes
+        ? result.data.purchaseOrderHeaders.nodes
+        : [];
       this.columns = this.data.length > 0 ? Object.keys(this.data[0]) : [];
     });
   }
